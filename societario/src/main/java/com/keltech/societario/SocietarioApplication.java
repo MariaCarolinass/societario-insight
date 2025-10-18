35a20c91b2c8dfd061a484c6f894f467b789cff0
mariacarolinass <m.carolina.soares1@gmail.com>
2025-10-18 11:39:27 -0300

    desenvolvi backend

---
+++ societario/src/main/java/com/keltech/societario/SocietarioApplication.java
@@ -0,0 +1,13 @@
+package com.keltech.societario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocietarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocietarioApplication.class, args);
+	}
+
+}
---
+++ societario/src/test/java/com/keltech/societario/SocietarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keltech.societario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocietarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
