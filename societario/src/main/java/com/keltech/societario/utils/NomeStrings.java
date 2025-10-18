c4bda77412ab1765d6614b934e8eb102ef3877b3
mariacarolinass <m.carolina.soares1@gmail.com>
2025-10-18 13:28:24 -0300

    add tratamento de strings

--- societario/src/main/java/com/keltech/societario/utils/NomeStrings.java
+++ societario/src/main/java/com/keltech/societario/utils/NomeStrings.java
@@ -7,9 +7,9 @@ public class NomeStrings {
     
     public String normalizarNome(String nome) {
         if (nome == null) return "";
-        String semAcento = java.text.Normalizer.normalize(nome, java.text.Normalizer.Form.NFD)
-                .replaceAll("[^\\p{ASCII}]", "");
-        return semAcento.trim().replaceAll("\\s+", " ").toUpperCase();
+        return nome.trim()
+                .replaceAll("\\s+", " ")
+                .toUpperCase();
     }
 
 }
